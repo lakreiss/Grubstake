f8db8a3f769f23934d2966ac9e95b00d06cc948f
Liam Kreiss <liam.kreiss@gmail.com>
2019-07-17 15:06:58 -0700

    transitioning to labels

--- gui.py
+++ gui.py
@@ -4,16 +4,16 @@ class GUI:
     def __init__(self):
         self.open_main_menu_gui()
 
-    def open_enter_order_gui(self, screen, frames_list):
+    def open_enter_order_gui(self, screen, frame_list, label_list):
         #TODO
-        for frame in frames_list:
+        for frame in frame_list:
             frame.destroy()
         # screen.quit()
         print("hello, world")
 
-    def open_main_menu_gui(self, screen="", frames_list=""):
-        if (frames_list is not ""):
-            for frame in frames_list:
+    def open_main_menu_gui(self, screen="", frame_list=""):
+        if (frame_list is not ""):
+            for frame in frame_list:
                 frame.destroy()
             main_screen = screen
         else:
@@ -28,26 +28,26 @@ class GUI:
         main_screen.geometry("%dx%d+0+0" % (self.window_w, self.window_h))
 
         # code to add widgets
-        frames_list = []
-        button_list = []
-        button_counter = 0
-        num_buttons = 2
+        frame_list = []
+        label_list = []
+        label_counter = 0
+        num_labels = 2
 
-        for i in range(num_buttons):
-            frames_list.append(tk.Frame(main_screen, width = (self.window_w / num_buttons) - (2 * num_buttons), height = self.window_h - 2))
-            frames_list[i].propagate(False)
-            frames_list[i].grid(row = 0, column = i, sticky = "nsew", padx = 2, pady = 2)
+        for i in range(num_labels):
+            frame_list.append(tk.Frame(main_screen, width = (self.window_w / num_labels) - (2 * num_labels), height = self.window_h - 2))
+            frame_list[i].propagate(False)
+            frame_list[i].grid(row = 0, column = i, sticky = "nsew", padx = 2, pady = 2)
 
-        enter_order_button = tk.Button(frames_list[button_counter], text="Enter Order", image=self.pixel, compound="c")
-        enter_order_button.pack(expand=True, fill="both")
-        enter_order_button.config(command = lambda: self.open_enter_order_gui(main_screen, frames_list))
-        button_list.append(enter_order_button)
-        button_counter += 1
+        enter_order_label = tk.Label(frame_list[label_counter], text="Enter Orders", width=100, height=100, compound="center")
+        enter_order_label.bind("<Button>", lambda e: self.open_enter_order_gui(screen, frame_list, label_list))
+        enter_order_label.pack(expand=True, fill="both")
+        label_list.append(enter_order_label)
+        label_counter += 1
 
-        view_orders_button = tk.Button(frames_list[button_counter], text="View Orders", image=self.pixel, compound="c")
-        view_orders_button.pack(expand=True, fill="both")
-        button_list.append(view_orders_button)
-        button_counter += 1
+        view_orders_label = tk.Label(frame_list[label_counter], text="View Orders", image=self.pixel, compound="c")
+        view_orders_label.pack(expand=True, fill="both")
+        label_list.append(view_orders_label)
+        label_counter += 1
 
         # main_screen.resizable(width=False, height=False)
         main_screen.mainloop()
