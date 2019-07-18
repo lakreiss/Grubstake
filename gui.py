229149b41082767a5407cfb389107b6027c5340a
Liam Kreiss <liam.kreiss@gmail.com>
2019-07-18 11:26:02 -0700

    added colors

--- gui.py
+++ gui.py
@@ -5,13 +5,21 @@ class GUI:
         self.units_file_name = "camp_info/units.txt"
         self.open_main_menu_gui()
 
-    def get_units_list(self):
-        f = open(self.units_file_name, "r")
+    #returns a frontend list and backend list of units, along with unit colors
+    def get_unit_list_and_colors(self):
+        units_file_name = "camp_info/units.txt"
+        f = open(units_file_name, "r")
         fl = f.readlines()
-        units_list = []
+        unit_list_frontend = []
+        unit_list_backend = []
+        color_list = []
         for line in fl:
-            units_list += [line.lower().replace(" ", "_").replace("\n", "")]
-        return units_list
+            unit, color = line.split("-")
+            unit_list_frontend += [unit]
+            unit_list_backend += [unit.lower().replace(" ", "_").replace("\n", "")]
+            color_list += [color.lower().replace(" ", "").replace("\n", "")]
+
+        return unit_list_frontend, unit_list_backend, color_list
 
     def open_enter_order_gui(self, screen, frame_list, label_list, order_info={}):
         #TODO
@@ -19,30 +27,43 @@ class GUI:
             frame.destroy()
 
         #get list of units
-        units_list = self.get_units_list()
+        unit_list_frontend, unit_list_backend, unit_colors = self.get_unit_list_and_colors()
 
         frame_list = []
         label_list = []
-        label_counter = 0
-        num_labels = len(units_list)
-        labels_per_line = 5
+        num_units = len(unit_list_frontend)
+        num_labels = num_units + 1
+        labels_per_line = 4
         num_lines = math.ceil(num_labels / labels_per_line)
 
         border = 2
         x_step = (self.window_w / (labels_per_line)) - border
         y_step = self.window_h / num_lines - border
-        for i in range(num_labels):
+        for i in range(num_units):
             frame_list.append(tk.Frame(screen, width = x_step, height = y_step))
             frame_list[i].propagate(False)
             frame_list[i].place(x = round((i % labels_per_line) * (x_step + 2)), y = round((i // labels_per_line) * (y_step + 2)))
 
-            enter_order_label = tk.Label(frame_list[label_counter], text=units_list[i], compound="c")
-            # enter_order_label.bind("<Button-" + str(i + 1) + ">", lambda e: self.open_choose_counselor_gui(screen, frame_list, label_list, order_info, units_list[i]))
-            enter_order_label.bind("<Button>", lambda e, unit_name=units_list[i]: self.open_choose_counselor_gui(screen, frame_list, label_list, order_info, unit_name))
-            enter_order_label.config(bg="gray")
+            enter_order_label = tk.Label(frame_list[i], text=unit_list_frontend[i], compound="c")
+            # enter_order_label.bind("<Button-" + str(i + 1) + ">", lambda e: self.open_choose_counselor_gui(screen, frame_list, label_list, order_info, unit_list_frontend[i]))
+            enter_order_label.bind("<Button>", lambda e, unit_name=unit_list_backend[i]: self.open_choose_counselor_gui(screen, frame_list, label_list, order_info, unit_name))
+            enter_order_label.config(bg=unit_colors[i])
             enter_order_label.pack(expand=True, fill="both")
             label_list.append(enter_order_label)
-            label_counter += 1
+
+        #back button
+        frame_list.append(tk.Frame(screen, width = x_step, height = y_step))
+        i = len(frame_list) - 1
+        frame_list[i].propagate(False)
+        frame_list[i].place(x = round((i % labels_per_line) * (x_step + 2)), y = round((i // labels_per_line) * (y_step + 2)))
+
+        enter_order_label = tk.Label(frame_list[i], text="Return", compound="c")
+        # enter_order_label.bind("<Button-" + str(i + 1) + ">", lambda e: self.open_choose_counselor_gui(screen, frame_list, label_list, order_info, unit_list_frontend[i]))
+        enter_order_label.bind("<Button>", lambda e: self.open_main_menu_gui(False, screen, frame_list, label_list))
+        enter_order_label.config(bg="red")
+        enter_order_label.pack(expand=True, fill="both")
+        label_list.append(enter_order_label)
+        i += 1
 
         # screen.quit()
         print("enter order clicked")
@@ -58,20 +79,22 @@ class GUI:
         # screen.quit()
         print("view orders clicked")
 
-    def open_main_menu_gui(self, screen="", frame_list=""):
-        if (frame_list is not ""):
-            for frame in frame_list:
-                frame.destroy()
-            main_screen = screen
-        else:
+    def open_main_menu_gui(self, new_menu=True, screen="", frame_list="", label_list=""):
+
+        if new_menu:
             main_screen = tk.Tk()
             main_screen.title("Grubstake")
 
-        self.screen_w, self.screen_h = main_screen.winfo_screenwidth(), main_screen.winfo_screenheight()
-        self.window_w, self.window_h = self.screen_w / 2, self.screen_h / 2
+            self.screen_w, self.screen_h = main_screen.winfo_screenwidth(), main_screen.winfo_screenheight()
+            self.window_w, self.window_h = self.screen_w / 2, self.screen_h / 2
 
-        #set window size
-        main_screen.geometry("%dx%d+0+0" % (self.window_w, self.window_h))
+            #set window size
+            main_screen.geometry("%dx%d+0+0" % (self.window_w, self.window_h))
+        else:
+            if (frame_list is not ""):
+                for frame in frame_list:
+                    frame.destroy()
+                main_screen = screen
 
         # code to add widgets
         frame_list = []
@@ -97,6 +120,6 @@ class GUI:
         label_list.append(view_order_label)
         label_counter += 1
 
-
-        # main_screen.resizable(width=False, height=False)
-        main_screen.mainloop()
+        if new_menu:
+            # main_screen.resizable(width=False, height=False)
+            main_screen.mainloop()
